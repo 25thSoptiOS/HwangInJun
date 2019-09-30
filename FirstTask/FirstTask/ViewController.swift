e62982c2b4fd53907a1a8722b8334056eb0608f4
Inzzoo <ghfdld@naver.com>
2019-09-30 18:30:46 +0900

    change button image state

--- FirstTask/FirstTask/ViewController.swift
+++ FirstTask/FirstTask/ViewController.swift
@@ -15,18 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var anythingImageView: UIImageView!
     @IBOutlet weak var heartButton: UIButton!
     let heartImage : UIImage = UIImage(named: "fullHeart.png")!
-    
+    let emptyHeartImage : UIImage = UIImage(named: "emptyHeart.png")!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        self.heartButton.setImage(heartImage, for: UIControl.State.selected)
+        self.heartButton.setImage(emptyHeartImage, for: UIControl.State.normal)
         //self.soptButton
         self.title = "Soptstargram"
     }
 
     @IBAction func touchHeartButton(_ sender: Any) {
         
-        self.heartButton.setImage(heartImage, for: UIControl.State.normal)
+        
         
         self.heartButton.setTitle("님이 좋아합니다.", for: UIControl.State.normal)
         
